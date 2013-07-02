d4df2798a0595992d860d031fdb1a7f501e44ef6
dhoko <dhoko@cyaneus.org>
2013-07-02 16:24:47 +0200

    Avancement generation

--- app.js
+++ app.js
@@ -19,7 +19,11 @@
 		}
 	];
  
-	var form = {
+	var Form = {
+
+		title : function(label) {
+			return '<h2>' + label + '</h2>';
+		}
 
 		input : function(label,name) {
 			var id = btoa(Math.floor(Math.random() * 1000));
@@ -27,16 +31,35 @@
 			_str += '<input type="radio" name="' + name + '" id="' + id + '" />';
 
 			return _str;
+		},
+
+		generate : function(config) {
+
+			var name = btoa(Math.floor(Math.random() * 1000) + 'rep');
+			var str = '<div id="' + name + '">';
+
+
+			for(dom in config) {
+				if('question' === dom) {
+					str += Form.title(config[dom]);
+				}
+
+				if('question' !== dom && 'valid' !== dom) {
+					str += Form.input(config[dom],name);
+				}
+			}
 		}
 
 	}
 
-	json.forEach(function(form,index) {
+	var formDiv = $('#contest');
 
-		var name = btoa(Math.floor(Math.random() * 1000) + 'rep');
-		console.log(form.valid);
+	json.forEach(function(ele,index) {
 
-		$('#input')
+		
+		console.log(ele.valid);
+		Form.generate(ele);
+		formDiv.html();
 	});
 
 })(jQuery);
