3e165d1e28eba78c64480e338059cd5381d6fe8e
dhoko <dhoko@cyaneus.org>
2013-07-02 17:43:46 +0200

    Builder pour le form

--- app.js
+++ app.js
@@ -19,12 +19,18 @@
 		}
 	];
  
+ 	/*
+		Form generator
+		Build a form from a custom JSON
+ 	*/
 	var Form = {
 
+		// Build the form title
 		title : function(label) {
 			return '<h2>' + label + '</h2>';
 		},
 
+		// Build radio input for a question
 		input : function(label,name) {
 			var id = btoa(Math.floor(Math.random() * 1000));
 			var _str = '<label for="' + id + '">' + label + '</label>';
@@ -33,16 +39,19 @@
 			return _str;
 		},
 
+		// Display the answer in the dom input:hidden
 		solution : function(label,name) {
 			return '<input type="hidden" value="' + label + '" id="rep' + name + '">'
 		},
 
+		// Main loop on each question in the JSON
 		generate : function(config) {
 
+			// Generate an id attach to the current step
 			var name = btoa(Math.floor(Math.random() * 1000) + 'rep');
 			var str = '<div id="' + name + '" class="formElement">';
 
-
+			// For each key build a custom element
 			for(dom in config) {
 				if('question' === dom) {
 					str += Form.title(config[dom]);
@@ -63,13 +72,25 @@
 
 	}
 
-	var formDiv = $('#contest');
+	var formMain = $('#contest');
 
-	json.forEach(function(ele,index) {
+	// OnLoad display only the first form
+	var display = function(div) {
 
-		formDiv.append(Form.generate(ele));
-	});
+		div.find('.formElement:not(.formElement:first-child)').addClass('hidden');
 
-	// $('.formElement:not(.formElement:first-child)').addClass('hidden')
+	}
+
+	// From the config to the dom, render the form
+	var builder = function(formDiv,callback) {
+
+		json.forEach(function(ele,index) {
+			formDiv.append(Form.generate(ele));
+		});
+
+		callback(formDiv);
+
+	}(formMain, display);
+
 
 })(jQuery);
